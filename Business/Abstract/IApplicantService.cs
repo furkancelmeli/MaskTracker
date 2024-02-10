e56b8b5343a747cff6383720c69a59f3d5f3831e
Furkan Çelmeli <celmelifurkan2@gmail.com>
2024-02-10 16:48:36 +0300

    Add project files.

---
+++ Business/Abstract/IApplicantService.cs
@@ -0,0 +1,27 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract;
+
+public interface IApplicantService
+{
+    void ApplyForMask(Person person)
+    {
+
+    }
+
+    public List<Person> GetList()
+    {
+        return null;
+    }
+
+    public bool CheckPerson(Person person)
+    {
+        return true;
+    }
+}
---
+++ Workspace/Program.cs
@@ -0,0 +1,18 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Person person1 = new Person();
+        person1.NationalIdentity = 12345678910;
+        person1.FirstName = "MUHAMMET FURKAN";
+        person1.LastName = "ÇELMELİ";
+        person1.DateOfBirthYear = 2000;
+
+        PttManager pttManager = new PttManager(new PersonManager());
+        pttManager.GiveMask(person1);
+        Console.ReadLine();
+    }
+}
